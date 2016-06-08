b25cffeb58faad46089b50f55952ba9831c7d7ff
TimSalimans <timsalimans@hotmail.com>
2016-06-08 17:32:09 +0200

    delete some unused code

--- nn.py
+++ nn.py
@@ -7,58 +7,7 @@ import lasagne
 from lasagne.layers import dnn
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
-def normalize_global_contrast(x):
-    if isinstance(x, np.ndarray):
-        s = x.shape
-        x = x.copy().reshape((s[0],np.prod(s[1:])))
-        x -= x.mean(axis=1).reshape((s[0],1))
-        x /= np.sqrt(np.mean(np.square(x),axis=1)).reshape((s[0],1))
-        return x.reshape(s)
-    elif isinstance(x, T.TensorVariable):
-        x -= T.mean(x,axis=(1,2,3),keepdims=True)
-        x /= T.sqrt(T.mean(T.square(x),axis=(1,2,3),keepdims=True))
-        return x
-    else:
-        raise NotImplementedError("Contrast normalization only implemented for numpy arrays or Theano TensorVariables")
-
-class ZCA(object):
-    def __init__(self, regularization=1e-5, x=None):
-        self.regularization = regularization
-        if x is not None:
-            self.fit(x)
-
-    def fit(self, x):
-        s = x.shape
-        x = x.copy().reshape((s[0],np.prod(s[1:])))
-        m = np.mean(x, axis=0)
-        x -= m
-        sigma = np.dot(x.T,x) / x.shape[0]
-        U, S, V = linalg.svd(sigma)
-        tmp = np.dot(U, np.diag(1./np.sqrt(S+self.regularization)))
-        tmp2 = np.dot(U, np.diag(np.sqrt(S+self.regularization)))
-        self.ZCA_mat = th.shared(np.dot(tmp, U.T).astype(th.config.floatX))
-        self.inv_ZCA_mat = th.shared(np.dot(tmp2, U.T).astype(th.config.floatX))
-        self.mean = th.shared(m.astype(th.config.floatX))
-
-    def apply(self, x):
-        s = x.shape
-        if isinstance(x, np.ndarray):
-            return np.dot(x.reshape((s[0],np.prod(s[1:]))) - self.mean.get_value(), self.ZCA_mat.get_value()).reshape(s)
-        elif isinstance(x, T.TensorVariable):
-            return T.dot(x.flatten(2) - self.mean.dimshuffle('x',0), self.ZCA_mat).reshape(s)
-        else:
-            raise NotImplementedError("Whitening only implemented for numpy arrays or Theano TensorVariables")
-            
-    def invert(self, x):
-        s = x.shape
-        if isinstance(x, np.ndarray):
-            return (np.dot(x.reshape((s[0],np.prod(s[1:]))), self.inv_ZCA_mat.get_value()) + self.mean.get_value()).reshape(s)
-        elif isinstance(x, T.TensorVariable):
-            return (T.dot(x.flatten(2), self.inv_ZCA_mat) + self.mean.dimshuffle('x',0)).reshape(s)
-        else:
-            raise NotImplementedError("Whitening only implemented for numpy arrays or Theano TensorVariables")
-
-# T.nnet.relu has some issues with very large inputs, this is more stable
+# T.nnet.relu has some stability issues, this is better
 def relu(x):
     return T.maximum(x, 0)
     
@@ -68,24 +17,6 @@ def lrelu(x, a=0.1):
 def log_sum_exp(x, axis=1):
     m = T.max(x, axis=axis)
     return m+T.log(T.sum(T.exp(x-m.dimshuffle(0,'x')), axis=axis))
-
-def adamax_updates(params, cost, lr=0.001, mom1=0.9, mom2=0.999):
-    updates = []
-    grads = T.grad(cost, params)
-    for p, g in zip(params, grads):
-        mg = th.shared(np.cast[th.config.floatX](p.get_value() * 0.))
-        v = th.shared(np.cast[th.config.floatX](p.get_value() * 0.))
-        if mom1>0:
-            v_t = mom1*v + (1. - mom1)*g
-            updates.append((v,v_t))
-        else:
-            v_t = g
-        mg_t = T.maximum(mom2*mg, abs(g))
-        g_t = v_t / (mg_t + 1e-6)
-        p_t = p - lr * g_t
-        updates.append((mg, mg_t))
-        updates.append((p, p_t))
-    return updates
 
 def adam_updates(params, cost, lr=0.001, mom1=0.9, mom2=0.999):
     updates = []
@@ -121,13 +52,6 @@ def l2normalize(layer, train_scale=True):
                           (k,), name="W_scale", trainable=train_scale, regularizable=False)
     layer.W = W_param * (layer.W_scale/T.sqrt(1e-6 + T.sum(T.square(W_param),axis=axes_to_sum))).dimshuffle(*dimshuffle_args)
     return layer
-    
-def softmax_loss(p_true, output_before_softmax):
-    output_before_softmax -= T.max(output_before_softmax, axis=1, keepdims=True)
-    if p_true.ndim==2:
-        return T.mean(T.log(T.sum(T.exp(output_before_softmax),axis=1)) - T.sum(p_true*output_before_softmax, axis=1))
-    else:
-        return T.mean(T.log(T.sum(T.exp(output_before_softmax),axis=1)) - output_before_softmax[T.arange(p_true.shape[0]),p_true])
 
 class BatchNormLayer(lasagne.layers.Layer):
     def __init__(self, incoming, b=lasagne.init.Constant(0.), g=lasagne.init.Constant(1.), nonlinearity=relu, **kwargs):
@@ -226,71 +150,7 @@ class GlobalAvgLayer(lasagne.layers.Layer):
         return T.mean(input, axis=(2,3))
     def get_output_shape_for(self, input_shape):
         return input_shape[:2]
-        
-class CenterScaleLayer(lasagne.layers.Layer):
-    def __init__(self, incoming, b=lasagne.init.Constant(0.), g=lasagne.init.Constant(1.), W=lasagne.init.Normal(0.05),
-                 train_g=True, nonlinearity=relu, **kwargs):
-        super(CenterScaleLayer, self).__init__(incoming, **kwargs)
-        self.nonlinearity = nonlinearity
-        k = self.input_shape[1]
-        if b is not None:
-            self.b = self.add_param(b, (k,), name="b", regularizable=False)
-        if g is not None:
-            self.g = self.add_param(g, (k,), name="g", regularizable=False, trainable=train_g)
-        self.avg_batch_mean = self.add_param(lasagne.init.Constant(0.), (k,), name="avg_batch_mean", regularizable=False, trainable=False)
-        if len(self.input_shape)==4:
-            self.axes_to_sum = (0,2,3)
-            self.dimshuffle_args = ['x',0,'x','x']
-        else:
-            self.axes_to_sum = 0
-            self.dimshuffle_args = ['x',0]
-        
-        # scale weights in layer below
-        incoming.W_param = incoming.W
-        incoming.W_param.set_value(W.sample(incoming.W_param.get_value().shape))
-        if incoming.W_param.ndim==4:
-            W_axes_to_sum = (1,2,3)
-            W_dimshuffle_args = [0,'x','x','x']
-        else:
-            W_axes_to_sum = 0
-            W_dimshuffle_args = ['x',0]
-        if g is not None:
-            incoming.W = incoming.W_param * (self.g/T.sqrt(T.sum(T.square(incoming.W_param),axis=W_axes_to_sum))).dimshuffle(*W_dimshuffle_args)
-        else:
-            incoming.W = incoming.W_param / T.sqrt(T.sum(T.square(incoming.W_param),axis=W_axes_to_sum,keepdims=True))        
 
-    def get_output_for(self, input, bn_mode, **kwargs):
-        if bn_mode=='avg':
-            activation = input - self.avg_batch_mean.dimshuffle(*self.dimshuffle_args)
-        elif bn_mode=='last':
-            activation = input - self.batch_mean.dimshuffle(*self.dimshuffle_args)
-        elif bn_mode in ['batch', 'batch_no_grad', 'init']:
-            self.batch_mean = T.mean(input,axis=self.axes_to_sum)
-            if bn_mode=='batch_no_grad':
-                self.batch_mean = th.gradient.disconnected_grad(self.batch_mean)
-            activation = input - self.batch_mean.dimshuffle(*self.dimshuffle_args)
-            self.bn_updates = [(self.avg_batch_mean, 0.9*self.avg_batch_mean + 0.1*self.batch_mean)]
-            if bn_mode=='init':
-                stdv = T.sqrt(T.mean(T.square(activation),axis=self.axes_to_sum))
-                activation /= stdv.dimshuffle(*self.dimshuffle_args)
-                self.init_updates = [(self.g, self.g/stdv)]
-        else:
-            raise NotImplementedError("bn_mode should be one of 'avg', 'last', 'batch', 'batch_no_grad', 'init'")
-            
-        if hasattr(self, 'b'):
-            activation += self.b.dimshuffle(*self.dimshuffle_args)
-            
-        return self.nonlinearity(activation)
-        
-def center_scale(layer, **kwargs):
-    nonlinearity = getattr(layer, 'nonlinearity', None)
-    if nonlinearity is not None:
-        layer.nonlinearity = lasagne.nonlinearities.identity
-    if hasattr(layer, 'b'):
-        del layer.params[layer.b]
-        layer.b = None
-    return CenterScaleLayer(layer, nonlinearity=nonlinearity, **kwargs)
-    
 class GaussianNoiseLayer(lasagne.layers.Layer):
     def __init__(self, incoming, sigma=0.1, **kwargs):
         super(GaussianNoiseLayer, self).__init__(incoming, **kwargs)
@@ -304,7 +164,6 @@ class GaussianNoiseLayer(lasagne.layers.Layer):
             if not use_last_noise:
                 self.noise = self._srng.normal(input.shape, avg=0.0, std=self.sigma)
             return input + self.noise
-    
 
 class L2NormLayer(lasagne.layers.Layer):
     def __init__(self, incoming, b=lasagne.init.Constant(0.), g=lasagne.init.Constant(1.),
